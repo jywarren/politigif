96690ce3d4e2dd4dda957ae6e16833793f24d163
jywarren <jeff@unterbahn.com>
2017-01-30 20:05:17 -0500

    style

--- dist/politigif.js
+++ dist/politigif.js
@@ -7,20 +7,21 @@ PolitiGif = function politiGif() {
   function capture(options){
     options = options || {};
     options.scaleFactor = options.scaleFactor || 0.5;
-    options.videoId = options.videoId || 'video';
-    options.outputId = options.outputId || 'output';
- 
+    options.videoId     = options.videoId     || 'video';
+    options.outputId    = options.outputId    || 'output';
+
     var video  = document.getElementById(options.videoId);
     var output = document.getElementById(options.outputId);
- 
+
     var canvas = videoToCanvas(video, options.scaleFactor);
- 
+
+    // does nothing?
     canvas.onclick = function(){
       window.open(this.toDataURL());
     };
- 
+
     overlayText("HELLO...", canvas, {});
- 
+
     output.appendChild(canvas);
   }
 
